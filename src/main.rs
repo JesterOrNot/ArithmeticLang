81cd4d1dea0e409f1ab6eea3f334067e2d3e857a
Sean Hellum <seanhellum45@gmail.com>
2020-02-22 23:07:11 +0000

    sums stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod lex;
-
+use crate::lex::Token;
 fn main() {
-    println!("Hello, world!");
+    let tokens = &lex::lex(&String::from("5 55 3 32 432  34")).unwrap();
+    let mut sum = 0;
+    for i in tokens.iter() {
+        match i {
+            Token::Number(n) => {
+                sum += n
+            },
+            _ => {}
+        }
+    }
+    println!("{}", sum)
 }
