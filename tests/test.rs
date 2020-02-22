8c3cec1db9977a315335fe2e9490317e4e0389b8
Sean Hellum <seanhellum45@gmail.com>
2020-02-22 22:18:56 +0000

    add a lexer and some unit tests

---
+++ tests/test.rs
@@ -0,0 +1,35 @@
+use lex::Token::CloseParenth;
+use lex::Token::Number;
+use lex::Token::OpenParenth;
+use lex::Token::Operator;
+use lex::Token::Whitespace;
+
+#[test]
+fn test_lex() {
+    println!("Test lex()");
+    assert_eq!(
+        lex::lex(&String::from("   ")).unwrap(),
+        vec![
+            lex::Token::Whitespace(' '),
+            lex::Token::Whitespace(' '),
+            lex::Token::Whitespace(' ')
+        ]
+    );
+    assert_eq!(
+        lex::lex(&String::from("535+5444  - 6 (4)")).unwrap(),
+        vec![
+            Number(535),
+            Operator('+'),
+            Number(5444),
+            Whitespace(' '),
+            Whitespace(' '),
+            Operator('-'),
+            Whitespace(' '),
+            Number(6),
+            Whitespace(' '),
+            OpenParenth('('),
+            Number(4),
+            CloseParenth(')')
+        ]
+    )
+}
